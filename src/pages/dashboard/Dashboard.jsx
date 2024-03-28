1ed4c7d9b6dfed6c94e1e978a412a1e15d10840f
Nurlan <nurlansirinov5@gmail.com>
2024-03-28 10:06:16 +0400

    nurlan/2803

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,49 +1,60 @@
 import React, { useState, useEffect } from 'react';
 import { Chart } from 'primereact/chart';
+import { GET_COUNT_DOCUMENTS } from '../../features/dashboard/services/api';
 
 function Dashboard() {
   const [chartData, setChartData] = useState({});
   const [chartOptions, setChartOptions] = useState({});
 
   useEffect(() => {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const data = {
-          labels: ['Gözləmədə', 'Tamamlandı', 'İmtina edildi', 'İlkin qeydiyyat'],
-          datasets: [
-              {
-                  data: [30, 13, 24, 7],
-                  backgroundColor: [
-                      '#0288d1',
-                      '#689f38',
-                      '#d32f2f',
-                      '#fbc02d' 
-                  ],
-                  hoverBackgroundColor: [
-                      documentStyle.getPropertyValue('--blue-400'),
-                      documentStyle.getPropertyValue('--green-400'),
-                      documentStyle.getPropertyValue('--red-500'),
-                      documentStyle.getPropertyValue('--yellow-400')
-                  ]
-              }
-          ]
-      };
-      const options = {
-          cutout: '60%'
-      };
+    async function fetchData() {
+      try {
+        const response = await GET_COUNT_DOCUMENTS();
+        const data = response.data;
+        const parsedData = JSON.parse(data);
 
-      setChartData(data);
-      setChartOptions(options);
+        const labels = parsedData.map(item => item.description);
+        const counts = parsedData.map(item => item.count);
+
+        const documentStyle = getComputedStyle(document.documentElement);
+
+        const chartData = {
+          labels: labels,
+          datasets: [
+            {
+              data: counts,
+              backgroundColor: [
+                '#0288d1',
+                '#689f38',
+                '#fbc02d'
+              ],
+              hoverBackgroundColor: [
+                documentStyle.getPropertyValue('--blue-400'),
+                documentStyle.getPropertyValue('--green-400'),
+                documentStyle.getPropertyValue('--yellow-400')
+              ]
+            }
+          ]
+        };
+
+        const chartOptions = {
+          cutout: '60%'
+        };
+        setChartData(chartData);
+        setChartOptions(chartOptions);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
   }, []);
 
   return (
-    <>
     <div className="card flex justify-content-center align-items-center mx-auto">
-          <Chart type="doughnut" data={chartData} options={chartOptions} className="w-full md:w-30rem" />
+      <Chart type="doughnut" data={chartData} options={chartOptions} className="w-full md:w-30rem" />
     </div>
-    </>
-  )
-
-
+  );
 }
 
 export default Dashboard;
